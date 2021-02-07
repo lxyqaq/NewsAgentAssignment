284b701ed4ff0ec3155724c3ffe477c58fc9f931
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-07 23:05:06 +0000

    Test

---
+++ NewsAgent/src/main/java/model/Test.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/7 23:04
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ NewsAgent/src/main/java/view/Test.java
@@ -0,0 +1,18 @@
+package view;
+
+import com.sun.tools.javac.Main;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/7 23:04
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+}
