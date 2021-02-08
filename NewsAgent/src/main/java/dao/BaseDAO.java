1373a38edfa057db97bba003e6739172bba19409
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-08 01:49:49 +0000

    BaseDao

---
+++ NewsAgent/src/main/java/dao/BaseDAO.java
@@ -0,0 +1,169 @@
+package dao;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName BaseDAO
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/8 01:34
+ * @Version 1.0
+ */
+public abstract class BaseDAO<T> {
+
+    private Class<T> clazz = null;
+
+    {
+        Type genericSuperclass = this.getClass().getGenericSuperclass();
+        ParameterizedType paramType = (ParameterizedType) genericSuperclass;
+
+        Type[] typeArguments = paramType.getActualTypeArguments();
+        clazz = (Class<T>) typeArguments[0];
+    }
+
+    /**
+     * @param conn
+     * @param sql
+     * @param args
+     * @return int
+     * @throws
+     * @description update method
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 01:45
+     */
+    public int update(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps);
+        }
+        return 0;
+    }
+
+    /**
+     * @param conn
+     * @param sql
+     * @param args
+     * @return T
+     * @throws
+     * @description Return a record in the data table
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 01:47
+     */
+    public T getInstance(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            if (rs.next()) {
+                T t = clazz.newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object columValue = rs.getObject(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                return t;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+        return null;
+    }
+
+    /**
+     * @param conn
+     * @param sql
+     * @param args
+     * @return java.util.List<T>
+     * @throws
+     * @description Returns a collection of multiple records in the data table
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 01:48
+     */
+    public List<T> getForList(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+
+            rs = ps.executeQuery();
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnCount = rsmd.getColumnCount();
+            ArrayList<T> list = new ArrayList<T>();
+            while (rs.next()) {
+                T t = clazz.newInstance();
+                for (int i = 0; i < columnCount; i++) {
+                    Object columValue = rs.getObject(i + 1);
+                    String columnLabel = rsmd.getColumnLabel(i + 1);
+                    Field field = clazz.getDeclaredField(columnLabel);
+                    field.setAccessible(true);
+                    field.set(t, columValue);
+                }
+                list.add(t);
+            }
+            return list;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+        return null;
+    }
+
+    /**
+     * @param conn
+     * @param sql
+     * @param args
+     * @return E
+     * @throws
+     * @description General method for querying special values
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 01:48
+     */
+    public <E> E getValue(Connection conn, String sql, Object... args) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i + 1, args[i]);
+            }
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return (E) rs.getObject(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(null, ps, rs);
+        }
+        return null;
+    }
+
+}
---
+++ NewsAgent/src/main/java/dao/JDBCUtils.java
@@ -0,0 +1,101 @@
+package dao;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+/**
+ * @ClassName JDBCUtils
+ * @Description TODO
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/8 01:22
+ * @Version 1.0
+ */
+public class JDBCUtils {
+
+    /**
+     * @description Use Druid database connection pool technology
+     * @param null
+     * @return null
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 01:30
+     */
+    private static DataSource source;
+
+    static {
+        try {
+            Properties pros = new Properties();
+
+            InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("druid.properties");
+
+            pros.load(is);
+
+            source = DruidDataSourceFactory.createDataSource(pros);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = source.getConnection();
+        return conn;
+    }
+
+    /**
+     * @param conn
+     * @param ps
+     * @description Operation of closing connection and Statement
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 23:02
+     */
+    public static void closeResource(Connection conn, Statement ps) {
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param conn
+     * @param ps
+     * @param rs
+     * @description Close resource operation
+     * @author lxyqaq @email A00279565@student.ait.ie
+     * @date 2021/2/8 23:45
+     */
+    public static void closeResource(Connection conn, Statement ps, ResultSet rs) {
+        try {
+            if (ps != null)
+                ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (conn != null)
+                conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (rs != null)
+                rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
