29d3a452b826fd8466be26989a924f791f061523
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-12 21:19:13 +0000

    Publication Bean

---
+++ NewsAgent/src/main/java/model/Publication.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * @ClassName Publication
+ * @Description Publication bean
+ * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Date 2021/2/12 21:17
+ * @Version 1.0
+ */
+public class Publication {
+
+
+
+}
