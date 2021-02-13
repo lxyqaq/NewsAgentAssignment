1f7cdc495f1b6af9c92ab744335613884c2981e6
Xiangyu Liu <lxyqaq0128@gmail.com>
2021-02-13 22:09:57 +0000

    update

---
+++ NewsAgent/src/main/java/controller/PublicationController.java
@@ -0,0 +1,107 @@
+package controller;
+
+import dao.PublicationDAO;
+import dao.PublicationDAOImpl;
+import model.Publication;
+import utils.JDBCUtils;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @ClassName PublicationController
+ * @Description TODO
+ * @Author Xiangyu Liu @Email A00279565@student.ait.ie
+ * @Date 2021/2/13 21:37
+ * @Version 1.0
+ */
+public class PublicationController {
+
+    private Scanner scanner = new Scanner(System.in);
+    private PublicationDAO publicationDAO = new PublicationDAOImpl();
+
+    // publicationDAO Method:
+    public void Insert() {
+        Connection conn = null;
+        System.out.printf("Enter Publication Name: \n");
+        String publicationName = scanner.next();
+        System.out.printf("Enter Publication amount: \n");
+        String publicationAmount = scanner.next();
+        try {
+            conn = JDBCUtils.getConnection();
+            Publication publi = new Publication(publicationName, publicationAmount);
+            publicationDAO.insert(conn, publi);
+            System.out.println("Added successfully");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    public void DeleteById() {
+        Connection conn = null;
+        System.out.printf("Enter Publication ID: \n");
+        int publicationId = scanner.nextInt();
+        try {
+            conn = JDBCUtils.getConnection();
+            publicationDAO.deleteById(conn, publicationId);
+            System.out.println("Successfully deleted");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    public void Update() {
+        Connection conn = null;
+        System.out.printf("Enter Publication ID: \n");
+        int publicationId = scanner.nextInt();
+        System.out.printf("Enter Publication Name: \n");
+        String publicationName = scanner.next();
+        System.out.printf("Enter Publication Amount: \n");
+        String publicationAmount = scanner.next();
+        try {
+            conn = JDBCUtils.getConnection();
+            Publication publi = new Publication(publicationId, publicationName, publicationAmount);
+            publicationDAO.update(conn, publi);
+            System.out.println("Successfully modified");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    public void GetPublicationById() {
+        Connection conn = null;
+        System.out.printf("Enter Publication ID: \n");
+        int publicationId = scanner.nextInt();
+        try {
+            conn = JDBCUtils.getConnection();
+            Publication publi = publicationDAO.getPublicationById(conn, publicationId);
+            System.out.println(publi);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+    public void GetAll() {
+        Connection conn = null;
+        try {
+            conn = JDBCUtils.getConnection();
+            List<Publication> list = publicationDAO.getAll(conn);
+            list.forEach(System.out::println);
+            System.out.println("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            JDBCUtils.closeResource(conn, null);
+        }
+    }
+
+}
--- NewsAgent/src/main/java/model/Publication.java
+++ NewsAgent/src/main/java/model/Publication.java
@@ -1,14 +1,106 @@
 package model;
 
+import controller.DaoExceptionHandler;
+
 /**
  * @ClassName Publication
  * @Description Publication bean
- * @Author lxyqaq @Email A00279565@student.ait.ie
+ * @Author Xiangyu Liu @Email A00279565@student.ait.ie
  * @Date 2021/2/12 21:17
  * @Version 1.0
  */
 public class Publication {
 
+    private int id;
+    private String name;
+    private String amount;
 
+    public Publication() {
+
+    }
+
+    public Publication(String name, String amount) throws DaoExceptionHandler {
+        try {
+            validateName(name);
+            validateAmount(amount);
+        } catch (DaoExceptionHandler daoExceptionHandler) {
+            daoExceptionHandler.printStackTrace();
+        }
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public Publication(int id, String name, String amount) {
+        this.id = id;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Publication{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
+
+    /**
+     * @param publicationrName
+     * @return void
+     * @throws
+     * @description validate publicationrName
+     * @author Xiangyu Liu @email A00279565@student.ait.ie
+     * @date 2021/2/13 21:20
+     */
+    public static void validateName(String publicationrName) throws DaoExceptionHandler {
+        if (publicationrName.isBlank() || publicationrName.isEmpty())
+            throw new DaoExceptionHandler("Publication Name NOT specified");
+        else if (publicationrName.length() < 2)
+            throw new DaoExceptionHandler("Publication Name does not meet minimum length requirements");
+        else if (publicationrName.length() > 45)
+            throw new DaoExceptionHandler("Publication Name does not exceeds maximum length requirements");
+    }
+
+    /**
+     * @param publicationAmount
+     * @return void
+     * @throws
+     * @description validate publicationAmount
+     * @author Xiangyu Liu @email A00279565@student.ait.ie
+     * @date 2021/2/13 21:20
+     */
+    public static void validateAmount(String publicationAmount) throws DaoExceptionHandler {
+        if (publicationAmount.isBlank() || publicationAmount.isEmpty())
+            throw new DaoExceptionHandler("Publication Name NOT specified");
+        else if (publicationAmount.length() < 1)
+            throw new DaoExceptionHandler("Publication Name does not meet minimum length requirements");
+        else if (publicationAmount.length() > 45)
+            throw new DaoExceptionHandler("Publication Name does not exceeds maximum length requirements");
+    }
 
 }
